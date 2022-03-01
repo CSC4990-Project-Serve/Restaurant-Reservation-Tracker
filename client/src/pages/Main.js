e5cf3c83e36760f2d4988864f2f1be4053d948ce
Jared Heeringa <jaredheeringa@icloud.com>
2022-03-01 15:57:25 -0600

    commented out unused import so react doesn't complain

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
-import { NavLink } from "react-router-dom";
-import {
-    SomonaukCountryKitchen,
-} from "../components";
+import React from 'react';
+import {NavLink} from "react-router-dom";
+// import {
+//     SomonaukCountryKitchen,
+// } from "../components";
 
 const Main = () => {
     return (
