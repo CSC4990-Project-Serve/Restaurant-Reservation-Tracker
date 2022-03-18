209c3fc15d534a8ad44d31ebee379542aa18611f
Jared Heeringa <jaredheeringa@icloud.com>
2022-03-17 21:36:02 -0500

    Added validation checks for updating and deleting users

--- api/controller/users.controller.js
+++ api/controller/users.controller.js
@@ -18,8 +18,9 @@ exports.getAllUsers = function (req, res) {
 // Add a new user to the database
 exports.createANewUser = function (req, res) {
     let newUser = new User(req.body);
+    console.log(newUser)
 
-    if (!newUser.username || !newUser.first_name || !newUser.last_name || !newUser.phone_number || !newUser.hashed_password) {
+    if (!newUser.username || !newUser.first_name || !newUser.last_name || !newUser.phone_number || !newUser.hashed_password || !newUser.password_salt) {
         res.status(400).send({error: true, message: 'Please provide full user information'});
     } else {
         User.crate_a_new_user(newUser, (err, results) => {
@@ -60,14 +61,15 @@ exports.getUserByID = (req, res) => {
 exports.updateUserByID = (req, res) => {
     let updatedUser = new User(req.body);
 
-    if (!updatedUser.username || !updatedUser.first_name || !updatedUser.last_name || !updatedUser.phone_number) {
+    // isNaN make sure the user id from the url is a number, if not throw the error below
+    if (!updatedUser.username || !updatedUser.first_name || !updatedUser.last_name || !updatedUser.phone_number || isNaN(req.params.id)) {
         res.status(400).send({error: true, status: "Error: Not fully detailed update"})
     } else {
         User.update_a_user(req.params.id, updatedUser, (err, results) => {
             if (err) {
                 res.send(err)
             } else {
-                res.json(results)
+                results ? res.send({error: false, status: `User ${req.params.id} updated`}) : res.send({error: true, status: `User not updated or not found`})
             }
         })
     }
@@ -76,7 +78,7 @@ exports.updateUserByID = (req, res) => {
 exports.deleteUserByID = (req, res) => {
     let userIDtoDelete = req.params.id;
 
-    if (!userIDtoDelete) {
+    if (!userIDtoDelete || isNaN(userIDtoDelete)) {
         res.status(400).send({error: true, status: "malformed input"})
     } else {
         User.delete_user_by_id(userIDtoDelete, (err, results) => {
