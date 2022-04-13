258300c5c6933a81db6b904ae118243a086544fe
Jared Heeringa <jaredheeringa@icloud.com>
2022-04-13 10:44:57 -0500

    Updated api port env

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // api project's .env file for environment variables
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.API_PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
