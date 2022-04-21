2f81c92e901e6750d942b5cb022b9584efbcc0c8
joancastro <castrojoan58@live.com>
2022-04-20 21:53:38 -0500

    GET request working for cards... need to limit amount of cards rendered

--- client/src/components/HomeSuggestions.js
+++ client/src/components/HomeSuggestions.js
@@ -2,12 +2,41 @@ import {Button, Card, CardGroup, Container} from "react-bootstrap";
 import ShowMore from "./ShowMore";
 import '../css/HomeSuggestions.css';
 import dummy_image from '../imgs/dummy-restaurant.jpg';
+import axios from "axios";
+import ReactDOM from "react-dom";
+import AdminTable from "./AdminTable";
+import {useEffect, useState} from "react";
 
+//TODO: get ID of individual card and have button navigate to individual restaurant page from click of button
 
 const HomeSuggestions = () => {
+    const[restaurant_data, setRestaurantData] = useState([]);
 
-    //TODO: get ID of individual card and have button navigate to individual restaurant page from click of button
-    //TODO: get search term from home page and search page re-search and display in "Results for" header
+    let restaurant_route = "http://localhost:5000/api/restaurant/";
+
+    useEffect(() => {
+        axios.get(restaurant_route)
+            .then(response => setRestaurantData(response.data));
+    }, []);
+
+    console.log(restaurant_data)
+
+    const HomeCards = restaurant_data.map(row => {
+        return (
+            <Card className="restaurant-cards" key={row.id}>
+                <Card.Img variant="top" className="restaurant-cards-img" src={dummy_image}/>
+                <Card.Body className="card-body-home">
+                    <Card.Title className="card-title-home">{row.restaurant_name}</Card.Title>
+                    <Card.Subtitle className="card-subtitle-home">{row.location.city}, {row.location.state}</Card.Subtitle>
+                    <Card.Text className="card-text-home">
+                        {row.restaurant_description}
+                    </Card.Text>
+                    <Button className="cards-button">Reserve Now</Button>
+                </Card.Body>
+            </Card>
+        )
+    })
+
 
     return (
         <>
@@ -15,20 +44,9 @@ const HomeSuggestions = () => {
             <Container>
                 <h2>Featured Restaurants</h2>
                 <CardGroup>
-                    {Array.from({length: 4}).map((_, index) => (
-                        <Card className="restaurant-cards" key={index}>
-                            <Card.Img variant="top" className="restaurant-cards-img" src={dummy_image}/>
-                            <Card.Body className="card-body-home">
-                                <Card.Title className="card-title-home">Restaurant Name</Card.Title>
-                                <Card.Subtitle className="card-subtitle-home">City, State</Card.Subtitle>
-                                <Card.Text className="card-text-home">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est iure, minima nam
-                                    recusandae repellat voluptatem.
-                                </Card.Text>
-                                <Button className="cards-button">Reserve Now</Button>
-                            </Card.Body>
-                        </Card>
-                    ))}
+
+                    {HomeCards}
+
                 </CardGroup>
             </Container>
 
