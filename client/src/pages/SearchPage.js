6860c8a3c983f3d2dbfbf4cd9b9e04dce6f287c8
Joan Castro <castrojoan58@live.com>
2022-04-21 11:12:10 -0500

    in progress GET request... search generating all data available, missing variables like review stars, images...

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,11 +1,12 @@
 import {Link, useLocation} from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
-import {Card, Col, Container, Row} from "react-bootstrap";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import {MDBIcon} from "mdb-react-ui-kit";
 import '../css/SearchPage.css';
 import dummy_image from '../imgs/dummy-restaurant.jpg';
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
+import axios from "axios";
 
 
 const SearchPage = () => {
@@ -23,6 +24,45 @@ const SearchPage = () => {
 
         setUserSearchTerm(form['searchInput'].value);
     }
+
+    const[restaurant_data, setRestaurantData] = useState([]);
+
+    let restaurant_route = "http://localhost:5000/api/restaurant/";
+
+    useEffect(() => {
+        axios.get(restaurant_route)
+            .then(response => setRestaurantData(response.data));
+    }, []);
+
+    const SearchCards = restaurant_data.map(row => {
+
+        return (
+            <Card type="button" className="card-container" key={row.id}>
+                <Row>
+                    <Col md={4}>
+                        <Card.Img variant="top" src={dummy_image}/>
+                    </Col>
+                    <Col md={8}>
+                        <Card.Body className="card-body-search">
+                            <Card.Title className="card-title-search">{row.restaurant_name}</Card.Title>
+                            <Card.Subtitle className="card-subtitle-search">{row.location.city}, {row.location.state}</Card.Subtitle>
+                            <Card.Text className="card-text-icons">
+                                <MDBIcon fas icon="star"/>
+                                <MDBIcon fas icon="star"/>
+                                <MDBIcon fas icon="star"/>
+                                <MDBIcon fas icon="star"/>
+                                <MDBIcon far icon="star"/>
+                            </Card.Text>
+                            <Card.Text className="card-text-search">
+                                {row.restaurant_description}
+                            </Card.Text>
+                        </Card.Body>
+                    </Col>
+                </Row>
+            </Card>
+        )
+    });
+
 
     return (
         <>
@@ -46,40 +86,10 @@ const SearchPage = () => {
 
             <Container>
                 {/*TODO: Fix routing for all rendering... needs to match actual back-end*/}
-                {Array.from({length: 4}).map((_, index) => (
-                    <Link to="/search/1" style={{textDecoration: 'none'}} key={index}>
-                        <Card type="button" className="card-container">
-                            <Row>
-                                <Col md={4}>
-                                    <Card.Img variant="top" src={dummy_image}/>
-                                </Col>
-                                <Col md={8}>
-                                    <Card.Body className="card-body-search">
-                                        <Card.Title className="card-title-search">Restaurant Name</Card.Title>
-                                        <Card.Subtitle className="card-subtitle-search">City, State</Card.Subtitle>
-                                        <Card.Text className="card-text-icons">
-                                            <MDBIcon fas icon="star"/>
-                                            <MDBIcon fas icon="star"/>
-                                            <MDBIcon fas icon="star"/>
-                                            <MDBIcon fas icon="star"/>
-                                            <MDBIcon far icon="star"/>
-                                        </Card.Text>
-                                        <Card.Text className="card-text-search">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-                                            blanditiis doloremque eaque enim eos eveniet fugiat illum incidunt iusto,
-                                            maiores molestiae nulla odit sunt suscipit tempora temporibus totam ullam
-                                            unde veniam vero! Accusamus architecto eveniet, ipsam laboriosam molestias
-                                            necessitatibus odit perferendis quod rem veritatis. Ea?
-                                        </Card.Text>
-                                        <Card.Text>
+                {SearchCards}
 
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Col>
-                            </Row>
-                        </Card>
-                    </Link>
-                ))}
+                    {/*<Link to="/search/1" style={{textDecoration: 'none'}} key={index}>*/}
+                    {/*</Link>*/}
             </Container>
             <Footer/>
         </>
