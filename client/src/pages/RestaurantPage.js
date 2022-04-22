1427c76b1a6c3960495d07be8eb4879ec88300b8
joancastro <castrojoan58@live.com>
2022-04-22 12:52:40 -0500

    structured components back into one, inside the return statement

--- client/src/pages/RestaurantPage.js
+++ client/src/pages/RestaurantPage.js
@@ -27,153 +27,126 @@ const RestaurantPage = () => {
         fetchData();
     }, []);
 
-    const LeftColumn = () => {
-        return (
-            <Col className="left-col-restaurant-details">
-                <h2 className="restaurant-details-heading">{restaurant_data.name}</h2>
-
-                <Carousel>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="carousel-img"
-                            src={carousel_img}
-                            alt="Overhead of Restaurant"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="carousel-img"
-                            src={carousel_img}
-                            alt="Overhead of Restaurant"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="carousel-img"
-                            src={carousel_img}
-                            alt="Overhead of Restaurant"
-                        />
-                    </Carousel.Item>
-                </Carousel>
-
-                <h2 className="restaurant-details-heading">About Us</h2>
-                <p>{restaurant_data.description}</p>
-            </Col>
-        );
-    }
-    const MiddleColumn = () => {
-        return (
-            <Col className="middle-col-restaurant-details">
-                <h2 className="restaurant-details-heading">Location</h2>
-                <p>{restaurant_data.address}, {restaurant_data.city}, {restaurant_data.state} {restaurant_data.postal_code}</p>
-
-                <h2 className="restaurant-details-heading">Hours of Operation</h2>
-                <p>Monday: {restaurant_data.mon}</p>
-                <p>Tuesday: {restaurant_data.tue}</p>
-                <p>Wednesday: {restaurant_data.wed}</p>
-                <p>Thursday: {restaurant_data.thu}</p>
-                <p>Friday: {restaurant_data.fri}</p>
-                <p>Saturday: {restaurant_data.sat}</p>
-                <p>Sunday: {restaurant_data.sun}</p>
-            </Col>
-        );
-    }
-    const RightColumn = () => {
-        return (
-            <Col className="right-col-restaurant-details">
-                <h2 className="restaurant-details-heading">Order now</h2>
-
-                {/*TODO: change format of time in database (varchar and HH:MM format)*/}
-                <p><MDBIcon fas icon="phone" className="phone-icon"/>&nbsp;&nbsp; {restaurant_data.phone}</p>
-
-                <div className="reservation-widget">
-                    <h2 className="reservation-heading">Make a Reservation</h2>
-                    <Form>
-                        <Row className="mb-3">
-                            <Col>
-                                <Form.Control type="text" placeholder="First name" />
-                            </Col>
-                            <Col>
-                                <Form.Control type="text" placeholder="Last name" />
-                            </Col>
-                        </Row>
-                        <Row className="mb-3">
-                            <Col>
-                                <Form.Control type="email" placeholder="Email" />
-                            </Col>
-                            <Col>
-                                <Form.Control type="tel" placeholder="Phone number" />
-                            </Col>
-                        </Row>
-                        <Row className="mb-3">
-                            <Col>
-                                <Form.Control type="number" placeholder="Party size" />
-                            </Col>
-                            <Col>
-                                <Form.Control type="text" placeholder="Occasion (optional)" />
-                            </Col>
-                        </Row>
-                        <Row className="mb-3">
-                            <Col>
-                                <Form.Control type="date" placeholder="Date" />
-                            </Col>
-                            <Col>
-                                <Form.Select>
-                                    <option>Time</option>
-                                    <option value="#">9:00 AM</option>
-                                    <option value="#">9:30 AM</option>
-                                    <option value="#">10:00 AM</option>
-                                    <option value="#">10:30 AM</option>
-                                    <option value="#">11:00 AM</option>
-                                    <option value="#">11:30 AM</option>
-                                    <option value="#">12:00 PM</option>
-                                    <option value="#">12:30 PM</option>
-                                    <option value="#">1:00 PM</option>
-                                    <option value="#">1:30 PM</option>
-                                    <option value="#">2:00 PM</option>
-                                    <option value="#">2:30 PM</option>
-                                    <option value="#">3:00 PM</option>
-                                    <option value="#">3:30 PM</option>
-                                    <option value="#">4:00 PM</option>
-                                    <option value="#">4:30 PM</option>
-                                    <option value="#">5:00 PM</option>
-                                    <option value="#">5:30 PM</option>
-                                    <option value="#">6:00 PM</option>
-                                    <option value="#">6:30 PM</option>
-                                    <option value="#">7:00 PM</option>
-                                </Form.Select>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Button className="reservation-button" type="submit">Complete Reservation</Button>
-                        </Row>
-                    </Form>
-                </div>
-            </Col>
-        );
-    }
-    const RestaurantDetails = () => {
-        return (
-            <Row>
-                {LeftColumn()}
-
-                {/*Split every individual column to see what was breaking the page*/}
-                {/*TODO: figure out why middle column is breaking page*/}
-                {MiddleColumn()}
-
-                {RightColumn()}
-            </Row>
-        )
-    }
-
     return (
         <>
             <NavigationBar />
 
             <Container className="restaurant-details-container">
+                <Row>
+                    <Col className="left-col-restaurant-details">
+                        <h2 className="restaurant-details-heading">{restaurant_data.name}</h2>
 
-                {RestaurantDetails()}
+                        <Carousel>
+                            <Carousel.Item interval={5000}>
+                                <img
+                                    className="carousel-img"
+                                    src={carousel_img}
+                                    alt="Overhead of Restaurant"
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item interval={5000}>
+                                <img
+                                    className="carousel-img"
+                                    src={carousel_img}
+                                    alt="Overhead of Restaurant"
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item interval={5000}>
+                                <img
+                                    className="carousel-img"
+                                    src={carousel_img}
+                                    alt="Overhead of Restaurant"
+                                />
+                            </Carousel.Item>
+                        </Carousel>
 
+                        <h2 className="restaurant-details-heading">About Us</h2>
+                        <p>{restaurant_data.description}</p>
+                    </Col>
+                    <Col className="middle-col-restaurant-details">
+                        <h2 className="restaurant-details-heading">Location</h2>
+                        <p>{restaurant_data.address}, {restaurant_data.city}, {restaurant_data.state} {restaurant_data.postal_code}</p>
+
+                        <h2 className="restaurant-details-heading">Hours of Operation</h2>
+                        <p>Monday: {restaurant_data.mon}</p>
+                        <p>Tuesday: {restaurant_data.tue}</p>
+                        <p>Wednesday: {restaurant_data.wed}</p>
+                        <p>Thursday: {restaurant_data.thu}</p>
+                        <p>Friday: {restaurant_data.fri}</p>
+                        <p>Saturday: {restaurant_data.sat}</p>
+                        <p>Sunday: {restaurant_data.sun}</p>
+                    </Col>
+                    <Col className="right-col-restaurant-details">
+                        <h2 className="restaurant-details-heading">Order now</h2>
+
+                        {/*TODO: change format of time in database (varchar and HH:MM format)*/}
+                        <p><MDBIcon fas icon="phone" className="phone-icon"/>&nbsp;&nbsp; {restaurant_data.phone}</p>
+
+                        <div className="reservation-widget">
+                            <h2 className="reservation-heading">Make a Reservation</h2>
+                            <Form>
+                                <Row className="mb-3">
+                                    <Col>
+                                        <Form.Control type="text" placeholder="First name" />
+                                    </Col>
+                                    <Col>
+                                        <Form.Control type="text" placeholder="Last name" />
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Col>
+                                        <Form.Control type="email" placeholder="Email" />
+                                    </Col>
+                                    <Col>
+                                        <Form.Control type="tel" placeholder="Phone number" />
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Col>
+                                        <Form.Control type="number" placeholder="Party size" />
+                                    </Col>
+                                    <Col>
+                                        <Form.Control type="text" placeholder="Occasion (optional)" />
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Col>
+                                        <Form.Control type="date" placeholder="Date" />
+                                    </Col>
+                                    <Col>
+                                        <Form.Select>
+                                            <option>Time</option>
+                                            <option value="#">9:00 AM</option>
+                                            <option value="#">9:30 AM</option>
+                                            <option value="#">10:00 AM</option>
+                                            <option value="#">10:30 AM</option>
+                                            <option value="#">11:00 AM</option>
+                                            <option value="#">11:30 AM</option>
+                                            <option value="#">12:00 PM</option>
+                                            <option value="#">12:30 PM</option>
+                                            <option value="#">1:00 PM</option>
+                                            <option value="#">1:30 PM</option>
+                                            <option value="#">2:00 PM</option>
+                                            <option value="#">2:30 PM</option>
+                                            <option value="#">3:00 PM</option>
+                                            <option value="#">3:30 PM</option>
+                                            <option value="#">4:00 PM</option>
+                                            <option value="#">4:30 PM</option>
+                                            <option value="#">5:00 PM</option>
+                                            <option value="#">5:30 PM</option>
+                                            <option value="#">6:00 PM</option>
+                                            <option value="#">6:30 PM</option>
+                                            <option value="#">7:00 PM</option>
+                                        </Form.Select>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Button className="reservation-button" type="submit">Complete Reservation</Button>
+                                </Row>
+                            </Form>
+                        </div>
+                    </Col>
+                </Row>
             </Container>
 
 
