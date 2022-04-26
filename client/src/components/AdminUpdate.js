7b78ea231759b8d936e99aa13b9b282a72a92cdf
joancastro <castrojoan58@live.com>
2022-04-26 18:17:51 -0500

    update and delete request implemented

--- client/src/components/AdminUpdate.js
+++ client/src/components/AdminUpdate.js
@@ -19,7 +19,6 @@ const AdminUpdate = (props) => {
     const[last_name, setLastName] = useState(old_last_name);
     const[phone_number, setUserPhone] = useState(old_phone_number);
 
-    // done
     const handleUserUpdate = () => {
         const updated_user = {id, username, email_address, first_name, last_name, phone_number};
 
@@ -28,8 +27,9 @@ const AdminUpdate = (props) => {
             headers: {"Content-type": "application/json" },
             body: JSON.stringify(updated_user)
         }).then(() => {
-            alert("Update successful.");
+            console.log("Update successful.");
         });
+        alert("Update successful.");
     }
     const UserUpdateForm = () => {
         return (
@@ -70,25 +70,23 @@ const AdminUpdate = (props) => {
     const[restaurant_name, setRestaurantName] = useState(old_restaurant_name);
     const[address1, setAddress1] = useState(old_address1);
     const[city, setCity] = useState(old_city);
-    const[restaurant_state, setRestaurantState] = useState(old_state);
+    const[state, setRestaurantState] = useState(old_state);
     const[restaurant_phone_number, setRestaurantPhone] = useState(old_restaurant_phone_number);
 
-    // ready, just need back-end
     const handleRestaurantUpdate = () => {
-        const updated_restaurant = {id, restaurant_name, address1, city, restaurant_state, restaurant_phone_number};
+        const updated_restaurant = {id, restaurant_name, address1, city, state, restaurant_phone_number};
 
         console.log(JSON.stringify(updated_restaurant));
 
-        // fetch(`http://localhost:5000/api/restaurant/${id}`, {
-        //     method: 'PUT',
-        //     headers: {"Content-type": "application/json" },
-        //     body: JSON.stringify(updated_user)
-        // }).then(() => {
-        //     alert("Update successful.");
-        // });
+        fetch(`http://localhost:5000/api/restaurant/${id}`, {
+            method: 'PUT',
+            headers: {"Content-type": "application/json" },
+            body: JSON.stringify(updated_restaurant)
+        }).then(() => {
+            console.log("Update successful.");
+        });
 
         alert("Update successful.");
-        window.location.reload();
     }
     const RestaurantUpdateForm = () => {
         return (
