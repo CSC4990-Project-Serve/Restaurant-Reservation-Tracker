04ea0fd255bf00c11126343c82fbdfc74a71fdb1
joancastro <castrojoan58@live.com>
2022-04-27 16:24:29 -0500

    previous version broke when clicking update on users for no apparent reason, rolled back to previous design with all parameters

--- client/src/components/AdminTable.js
+++ client/src/components/AdminTable.js
@@ -5,7 +5,14 @@ import {Form} from "react-bootstrap";
 const AdminTable = (props) => {
     const {restaurant_data, user_data} = props;
     const[category, setCategory] = useState("Restaurants");
-    const[updateActive, setUpdateActive] = useState({ update: false, id: 0, old_user_data: "", old_restaurant_data: ""});
+    const[updateActive, setUpdateActive] = useState(
+        {
+            update: false, id: 0,
+            username: "", email_address: "", first_name: "", last_name: "", phone_number: "",
+            restaurant_name: "", restaurant_description: "", star_rating: "", restaurant_phone_number: "",
+            address1: "", city: "", state: "", postal_code: "", country: "",
+            monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "", sunday: ""
+        });
 
     const RestaurantHeading = () => {
         return (
@@ -29,10 +36,10 @@ const AdminTable = (props) => {
                 <td>{row.location.state}</td>
                 <td>{row.restaurant_phone_number}</td>
                 <td className="td-button">
-                    <button key={row.id} type="button" className="btn btn-success" onClick={() => handleRestaurantUpdate(row.id, row)}>Update</button>
+                    <button key={row.id} type="button" className="btn btn-success" onClick={() => handleRestaurantUpdate(row.id, row.restaurant_name, row.restaurant_description, row.star_rating, row.restaurant_phone_number, row.location.address1, row.location.city, row.location.state, row.location.postal_code, row.location.country, row.hours.monday, row.hours.tuesday, row.hours.wednesday, row.hours.thursday, row.hours.friday, row.hours.saturday, row.hours.sunday)}>Update</button>
                 </td>
                 <td className="td-button">
-                    <button key={row.id} type="button" className="btn btn-danger" onClick={() => handleRestaurantDelete(row.id)}>Delete</button>
+                    <button key={row.id} type="button" className="btn btn-danger" onClick={() => handleRestaurantDelete(row.id, )}>Delete</button>
                 </td>
             </tr>
         )
@@ -45,8 +52,15 @@ const AdminTable = (props) => {
             window.location.reload();
         });
     }
-    const handleRestaurantUpdate = (id, row) => {
-        setUpdateActive({update: true, id: id, old_restaurant_data: row, old_user_data: ""});
+    const handleRestaurantUpdate = (id, restaurant_name, restaurant_description, star_rating, restaurant_phone_number, address1, city, state, postal_code, country, monday, tuesday, wednesday, thursday, friday, saturday, sunday) => {
+        setUpdateActive(
+            {
+                update: true, id: id,
+                restaurant_name: restaurant_name, restaurant_description: restaurant_description, star_rating: star_rating, restaurant_phone_number: restaurant_phone_number,
+                address1: address1, city: city, state: state, postal_code: postal_code, country: country,
+                monday: monday, tuesday: tuesday, wednesday: wednesday, thursday: thursday, friday: friday, saturday: saturday, sunday: sunday,
+                username: "", email_address: "", first_name: "", last_name: "", phone_number: ""
+            });
     }
 
     const UserHeading = () => {
@@ -71,7 +85,7 @@ const AdminTable = (props) => {
                 <td>{row.last_name}</td>
                 <td>{row.phone_number}</td>
                 <td className="td-button">
-                    <button key={row.id} type="button" className="btn btn-success" onClick={() => handleUserUpdate(row.id, row)}>Update</button>
+                    <button key={row.id} type="button" className="btn btn-success" onClick={() => handleUserUpdate(row.id, row.username, row.email_address, row.first_name, row.last_name, row.phone_number)}>Update</button>
                 </td>
                 <td className="td-button">
                     <button key={row.id} type="button" className="btn btn-danger" onClick={() => handleUserDelete(row.id)}>Delete</button>
@@ -88,8 +102,15 @@ const AdminTable = (props) => {
         });
 
     }
-    const handleUserUpdate = (id, row) => {
-        setUpdateActive({update: true, id: id, old_user_data: row, old_restaurant_data: ""});
+    const handleUserUpdate = (id, username, email_address, first_name, last_name, phone_number) => {
+        setUpdateActive(
+            {
+                update: true, id: id,
+                username: username, email_address: email_address, first_name: first_name, last_name: last_name, phone_number: phone_number,
+                restaurant_name: "", restaurant_description: "", star_rating: "", restaurant_phone_number: "",
+                address1: "", city: "", state: "", postal_code: "", country: "",
+                monday: "", tuesday: "", wednesday: "", thursday: "", friday: "", saturday: "", sunday: ""
+            });
     }
 
     return (
@@ -104,10 +125,10 @@ const AdminTable = (props) => {
 
             <table className="admin-page-table">
                 <thead>
-                    {category === "Restaurants" ? RestaurantHeading() : UserHeading()}
+                {category === "Restaurants" ? RestaurantHeading() : UserHeading()}
                 </thead>
                 <tbody>
-                    {category === "Restaurants" ? RestaurantData : UserData}
+                {category === "Restaurants" ? RestaurantData : UserData}
                 </tbody>
             </table>
 
@@ -115,8 +136,10 @@ const AdminTable = (props) => {
                 {updateActive.update === true &&
                     <AdminUpdate
                         choice={category} update={updateActive.update}  id={updateActive.id}
-                        old_user_data={updateActive.old_user_data}
-                        old_restaurant_data={updateActive.old_restaurant_data}
+                        old_username={updateActive.username} old_email_address={updateActive.email_address} old_first_name={updateActive.first_name} old_last_name={updateActive.last_name} old_phone_number={updateActive.phone_number}
+                        old_restaurant_name={updateActive.restaurant_name} old_restaurant_description={updateActive.restaurant_description} old_restaurant_phone_number={updateActive.restaurant_phone_number} old_star_rating={updateActive.star_rating}
+                        old_address1={updateActive.address1} old_city={updateActive.city} old_state={updateActive.state} old_postal_code={updateActive.postal_code} old_country={updateActive.country}
+                        old_monday={updateActive.monday} old_tuesday={updateActive.tuesday} old_wednesday={updateActive.wednesday} old_thursday={updateActive.thursday} old_friday={updateActive.friday} old_saturday={updateActive.saturday} old_sunday={updateActive.sunday}
                     />
                 }
             </div>
