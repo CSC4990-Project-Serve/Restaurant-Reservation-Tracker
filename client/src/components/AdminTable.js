f4b17fde9fd28dcf3ccbda830a28018662de6211
joancastro <castrojoan58@live.com>
2022-04-27 14:32:44 -0500

    parameters that are not included in update form now keep previous state

--- client/src/components/AdminTable.js
+++ client/src/components/AdminTable.js
@@ -5,7 +5,7 @@ import {Form} from "react-bootstrap";
 const AdminTable = (props) => {
     const {restaurant_data, user_data} = props;
     const[category, setCategory] = useState("Restaurants");
-    const[updateActive, setUpdateActive] = useState({ update: false, id: 0, username: "", email_address: "", first_name: "", last_name: "", phone_number: "", restaurant_name: "",  address1: "", city: "", state: "", restaurant_phone_number: ""});
+    const[updateActive, setUpdateActive] = useState({ update: false, id: 0, old_user_data: "", old_restaurant_data: ""});
 
     const RestaurantHeading = () => {
         return (
@@ -29,10 +29,10 @@ const AdminTable = (props) => {
                 <td>{row.location.state}</td>
                 <td>{row.restaurant_phone_number}</td>
                 <td className="td-button">
-                    <button key={row.id} type="button" className="btn btn-success" onClick={(e) => handleRestaurantUpdate(row.id, row.restaurant_name, row.location.address1, row.location.city, row.location.state, row.restaurant_phone_number, e)}>Update</button>
+                    <button key={row.id} type="button" className="btn btn-success" onClick={() => handleRestaurantUpdate(row.id, row)}>Update</button>
                 </td>
                 <td className="td-button">
-                    <button key={row.id} type="button" className="btn btn-danger" onClick={(e) => handleRestaurantDelete(row.id, e)}>Delete</button>
+                    <button key={row.id} type="button" className="btn btn-danger" onClick={() => handleRestaurantDelete(row.id)}>Delete</button>
                 </td>
             </tr>
         )
@@ -45,8 +45,8 @@ const AdminTable = (props) => {
             window.location.reload();
         });
     }
-    const handleRestaurantUpdate = (id, restaurant_name, address1, city, state, restaurant_phone_number) => {
-        setUpdateActive({update: true, id: id, restaurant_name: restaurant_name, address1: address1, city: city, state: state, restaurant_phone_number: restaurant_phone_number, username: "", email_address: "", first_name: "", last_name: "", phone_number: ""});
+    const handleRestaurantUpdate = (id, row) => {
+        setUpdateActive({update: true, id: id, old_restaurant_data: row, old_user_data: ""});
     }
 
     const UserHeading = () => {
@@ -71,7 +71,7 @@ const AdminTable = (props) => {
                 <td>{row.last_name}</td>
                 <td>{row.phone_number}</td>
                 <td className="td-button">
-                    <button key={row.id} type="button" className="btn btn-success" onClick={() => handleUserUpdate(row.id, row.username, row.email_address, row.first_name, row.last_name, row.phone_number)}>Update</button>
+                    <button key={row.id} type="button" className="btn btn-success" onClick={() => handleUserUpdate(row.id, row)}>Update</button>
                 </td>
                 <td className="td-button">
                     <button key={row.id} type="button" className="btn btn-danger" onClick={() => handleUserDelete(row.id)}>Delete</button>
@@ -88,8 +88,8 @@ const AdminTable = (props) => {
         });
 
     }
-    const handleUserUpdate = (id, username, email_address, first_name, last_name, phone_number) => {
-        setUpdateActive({update: true, id: id, username: username, email_address: email_address, first_name: first_name, last_name: last_name, phone_number: phone_number, restaurant_name: "", address1: "", city: "", state: "", restaurant_phone_number: ""});
+    const handleUserUpdate = (id, row) => {
+        setUpdateActive({update: true, id: id, old_user_data: row, old_restaurant_data: ""});
     }
 
     return (
@@ -115,8 +115,8 @@ const AdminTable = (props) => {
                 {updateActive.update === true &&
                     <AdminUpdate
                         choice={category} update={updateActive.update}  id={updateActive.id}
-                        old_username={updateActive.username} old_email_address={updateActive.email_address} old_first_name={updateActive.first_name} old_last_name={updateActive.last_name} old_phone_number={updateActive.phone_number}
-                        old_restaurant_name={updateActive.restaurant_name} old_address1={updateActive.address1} old_city={updateActive.city} old_state={updateActive.state} old_restaurant_phone_number={updateActive.restaurant_phone_number}
+                        old_user_data={updateActive.old_user_data}
+                        old_restaurant_data={updateActive.old_restaurant_data}
                     />
                 }
             </div>
--- client/src/components/AdminUpdate.js
+++ client/src/components/AdminUpdate.js
@@ -6,18 +6,18 @@ const AdminUpdate = (props) => {
     let {choice, update, id} = props;
 
     if(update === true) {
-        window.scrollTo(0,1000);
+        window.scrollTo(0, document.body.scrollHeight);
     }  else {
         window.scrollTo(0,0);
     }
 
     // User Properties:
-    const {old_username, old_email_address, old_first_name, old_last_name, old_phone_number} = props;
-    const[username, setUsername] = useState(old_username);
-    const[email_address, setEmail] = useState(old_email_address);
-    const[first_name, setFirstName] = useState(old_first_name);
-    const[last_name, setLastName] = useState(old_last_name);
-    const[phone_number, setUserPhone] = useState(old_phone_number);
+    const {old_user_data} = props;
+    const[username, setUsername] = useState(old_user_data.username);
+    const[email_address, setEmail] = useState(old_user_data.email_address);
+    const[first_name, setFirstName] = useState(old_user_data.first_name);
+    const[last_name, setLastName] = useState(old_user_data.last_name);
+    const[phone_number, setUserPhone] = useState(old_user_data.phone_number);
 
     const handleUserUpdate = () => {
         const updated_user = {id, username, email_address, first_name, last_name, phone_number};
@@ -39,23 +39,23 @@ const AdminUpdate = (props) => {
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="username">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" defaultValue={old_username} onChange= {(e) => setUsername(e.target.value)} />
+                            <Form.Control type="text" defaultValue={old_user_data.username} onChange= {(e) => setUsername(e.target.value)} />
                         </Form.Group>
                         <Form.Group as={Col} controlId="email">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" defaultValue={old_email_address} onChange= {(e) => setEmail(e.target.value)} />
+                            <Form.Control type="email" defaultValue={old_user_data.email_address} onChange= {(e) => setEmail(e.target.value)} />
                         </Form.Group>
                         <Form.Group as={Col} controlId="first_name">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" defaultValue={old_first_name} onChange= {(e) => setFirstName(e.target.value)} />
+                            <Form.Control type="text" defaultValue={old_user_data.first_name} onChange= {(e) => setFirstName(e.target.value)} />
                         </Form.Group>
                         <Form.Group as={Col} controlId="last_name">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="text" defaultValue={old_last_name} onChange= {(e) => setLastName(e.target.value)} />
+                            <Form.Control type="text" defaultValue={old_user_data.last_name} onChange= {(e) => setLastName(e.target.value)} />
                         </Form.Group>
                         <Form.Group as={Col} controlId="phone_number">
                             <Form.Label>Phone Number</Form.Label>
-                            <Form.Control type="text" defaultValue={old_phone_number} onChange= {(e) => setUserPhone(e.target.value)} />
+                            <Form.Control type="text" defaultValue={old_user_data.phone_number} onChange= {(e) => setUserPhone(e.target.value)} />
                         </Form.Group>
                     </Row>
 
@@ -66,17 +66,32 @@ const AdminUpdate = (props) => {
     }
 
     // Restaurant Properties:
-    const {old_restaurant_name, old_address1, old_city, old_state, old_restaurant_phone_number} = props;
-    const[restaurant_name, setRestaurantName] = useState(old_restaurant_name);
-    const[address1, setAddress1] = useState(old_address1);
-    const[city, setCity] = useState(old_city);
-    const[state, setRestaurantState] = useState(old_state);
-    const[restaurant_phone_number, setRestaurantPhone] = useState(old_restaurant_phone_number);
+    const {old_restaurant_data} = props;
+    const[restaurant_name, setRestaurantName] = useState(old_restaurant_data.restaurant_name);
+    const[restaurant_description, setRestaurantDescription] = useState(old_restaurant_data.restaurant_description);
+    const[restaurant_phone_number, setRestaurantPhone] = useState(old_restaurant_data.restaurant_phone_number);
+    const[star_rating, setStarRating] = useState(old_restaurant_data.star_rating);
+
+    const[address1, setAddress1] = useState(old_restaurant_data.location.address1);
+    const[city, setCity] = useState(old_restaurant_data.location.city);
+    const[state, setRestaurantState] = useState(old_restaurant_data.location.state);
+    const[postal_code, setPostalCode] = useState(old_restaurant_data.location.postal_code);
+    const country = old_restaurant_data.location.country;
+
+    const monday = old_restaurant_data.hours.monday;
+    const tuesday = old_restaurant_data.hours.tuesday;
+    const wednesday = old_restaurant_data.hours.wednesday;
+    const thursday = old_restaurant_data.hours.thursday;
+    const friday = old_restaurant_data.hours.friday;
+    const saturday = old_restaurant_data.hours.saturday;
+    const sunday = old_restaurant_data.hours.sunday;
 
     const handleRestaurantUpdate = () => {
-        const updated_restaurant = {id, restaurant_name, address1, city, state, restaurant_phone_number};
-
-        console.log(JSON.stringify(updated_restaurant));
+        const updated_restaurant = {
+            id, restaurant_name, restaurant_description, restaurant_phone_number, star_rating,
+            address1, city, state, postal_code, country,
+            monday, tuesday, wednesday, thursday, friday, saturday, sunday
+        };
 
         fetch(`http://localhost:5000/api/restaurant/${id}`, {
             method: 'PUT',
@@ -96,23 +111,40 @@ const AdminUpdate = (props) => {
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="restaurant_name">
                             <Form.Label>Restaurant Name</Form.Label>
-                            <Form.Control type="text" defaultValue={old_restaurant_name} onChange= {(e) => setRestaurantName(e.target.value)} />
+                            <Form.Control type="text" defaultValue={old_restaurant_data.restaurant_name} onChange= {(e) => setRestaurantName(e.target.value)} />
                         </Form.Group>
+
+                        <Form.Group as={Col} controlId="restaurant_phone_number">
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control type="text" defaultValue={old_restaurant_data.restaurant_phone_number} onChange= {(e) => setRestaurantPhone(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="star_rating">
+                            <Form.Label>Average Review</Form.Label>
+                            <Form.Control type="number" min="1" max="5" defaultValue={old_restaurant_data.star_rating} onChange= {(e) => setStarRating(e.target.value)} />
+                        </Form.Group>
+                    </Row>
+                    <Row className="mb-3">
+                        <Form.Group className="mb-3" as={Col} controlId="restaurant_description">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control as="textarea" placeholder="Enter description" defaultValue={old_restaurant_data.restaurant_description} onChange= {(e) => setRestaurantDescription(e.target.value)} />
+                        </Form.Group>
+                    </Row>
+                    <Row className="mb-3">
                         <Form.Group as={Col} controlId="address1">
                             <Form.Label>Address</Form.Label>
-                            <Form.Control type="text" defaultValue={old_address1} onChange= {(e) => setAddress1(e.target.value)} />
+                            <Form.Control type="text" defaultValue={old_restaurant_data.location.address1} onChange= {(e) => setAddress1(e.target.value)} />
                         </Form.Group>
                         <Form.Group as={Col} controlId="city">
                             <Form.Label>City</Form.Label>
-                            <Form.Control type="text" defaultValue={old_city} onChange= {(e) => setCity(e.target.value)} />
+                            <Form.Control type="text" defaultValue={old_restaurant_data.location.city} onChange= {(e) => setCity(e.target.value)} />
                         </Form.Group>
                         <Form.Group as={Col} controlId="state">
                             <Form.Label>State</Form.Label>
-                            <Form.Control type="text" defaultValue={old_state} onChange= {(e) => setRestaurantState(e.target.value)} />
+                            <Form.Control type="text" defaultValue={old_restaurant_data.location.state} onChange= {(e) => setRestaurantState(e.target.value)} />
                         </Form.Group>
-                        <Form.Group as={Col} controlId="restaurant_phone_number">
-                            <Form.Label>Phone Number</Form.Label>
-                            <Form.Control type="text" defaultValue={old_restaurant_phone_number} onChange= {(e) => setRestaurantPhone(e.target.value)} />
+                        <Form.Group as={Col} controlId="postal_code">
+                            <Form.Label>Postal Code</Form.Label>
+                            <Form.Control type="text" defaultValue={old_restaurant_data.location.postal_code} onChange= {(e) => setPostalCode(e.target.value)} />
                         </Form.Group>
                     </Row>
 
