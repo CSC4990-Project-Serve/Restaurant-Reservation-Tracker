7e46b463999eace89d04d7fa86fdb5733380d737
Jared Heeringa <jaredheeringa@icloud.com>
2022-04-28 14:57:09 -0500

    Added api request limiter

--- api/server.js
+++ api/server.js
@@ -1,8 +1,20 @@
 require('dotenv').config(); // api project's .env file for environment variables
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit') // set up rate limiter: maximum of five requests per minute
 const app = express();
 const port = process.env.API_PORT || 5000;
+
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
